1d5346aeb5dfed51534a5f18df7e0731610988a8
yoseph0524 <yoseph0524@g.ucla.edu>
2023-09-26 22:45:15 -0700

    react query

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,6 +2,8 @@ import { Box, Grid, HStack, Skeleton, SkeletonText } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import Room from "../components/Room";
 import RoomSkeleton from "../components/RoomSkeleton";
+import { useQuery } from "@tanstack/react-query";
+import { getRooms } from "../api";
 
 interface IPhoto {
   pk: string;
@@ -21,17 +23,7 @@ interface IRoom {
 }
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [rooms, setRooms] = useState<IRoom[]>([]);
-  const fetchRooms = async () => {
-    const response = await fetch("http://127.0.0.1:8000/api/v1/rooms/");
-    const json = await response.json();
-    setRooms(json);
-    setIsLoading(false);
-  };
-  useEffect(() => {
-    fetchRooms();
-  }, []);
+  const { isLoading, data } = useQuery<IRoom[]>(["rooms"], getRooms);
   return (
     <Grid
       mt={10}
@@ -63,7 +55,7 @@ export default function Home() {
           <RoomSkeleton />
         </>
       ) : null}
-      {rooms.map((room) => (
+      {data?.map((room) => (
         <Room
           imageUrl={
             room.photos[0].file ?? "https://source.unsplash.com/random/450x450"
