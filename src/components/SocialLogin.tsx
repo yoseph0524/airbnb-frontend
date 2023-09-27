fbd51dcc956198dfcd6d8a5eff93bd5d3a04b381
yoseph0524 <yoseph0524@g.ucla.edu>
2023-09-27 14:14:40 -0700

    github login

--- src/components/SocialLogin.tsx
+++ src/components/SocialLogin.tsx
@@ -12,7 +12,13 @@ export default function SocialLogin() {
         <Divider />
       </HStack>
       <VStack>
-        <Button w="100%" leftIcon={<FaGithub />} colorScheme={"telegram"}>
+        <Button
+          as="a"
+          href="https://github.com/login/oauth/authorize?client_id=c3d4825913157273a0e7&scope=read:user,user:email"
+          w="100%"
+          leftIcon={<FaGithub />}
+          colorScheme={"telegram"}
+        >
           Continue with Github
         </Button>
         <Button w="100%" leftIcon={<FaComment />} colorScheme={"yellow"}>
