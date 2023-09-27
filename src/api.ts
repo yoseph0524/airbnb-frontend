99b26d004c5c014c5bb06de6af31ee999c5f5ba3
yoseph0524 <yoseph0524@g.ucla.edu>
2023-09-27 12:25:59 -0700

    log out

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,5 @@
-import { QueryFunctionContext, useQuery } from "@tanstack/react-query";
+import Cookie from "js-cookie";
+import { QueryFunctionContext } from "@tanstack/react-query";
 import axios from "axios";
 
 const instance = axios.create({
@@ -23,3 +24,12 @@ export const getRoomReviews = ({ queryKey }: QueryFunctionContext) => {
 
 export const getMe = () =>
   instance.get(`users/me`).then((response) => response.data);
+
+export const logOut = () =>
+  instance
+    .post(`users/log-out`, null, {
+      headers: {
+        "X-CSRFToken": Cookie.get("csrftoken") || "",
+      },
+    })
+    .then((response) => response.data);
