f22b4bf16cff75d87270a7e05ac5b0457bf0b9bc
yoseph0524 <yoseph0524@g.ucla.edu>
2023-09-27 14:50:30 -0700

    kakao login

--- src/components/SocialLogin.tsx
+++ src/components/SocialLogin.tsx
@@ -2,6 +2,12 @@ import { FaComment, FaGithub } from "react-icons/fa";
 import { Box, Button, Divider, HStack, Text, VStack } from "@chakra-ui/react";
 
 export default function SocialLogin() {
+  const kakaoParams = {
+    client_id: "66c1431c6c614ff25984da2358b46496",
+    redirect_uri: "http://127.0.0.1:3000/social/kakao",
+    response_type: "code",
+  };
+  const params = new URLSearchParams(kakaoParams).toString();
   return (
     <Box mb={4}>
       <HStack my={8}>
@@ -21,7 +27,13 @@ export default function SocialLogin() {
         >
           Continue with Github
         </Button>
-        <Button w="100%" leftIcon={<FaComment />} colorScheme={"yellow"}>
+        <Button
+          as="a"
+          href={`https://kauth.kakao.com/oauth/authorize?${params}`}
+          w="100%"
+          leftIcon={<FaComment />}
+          colorScheme={"yellow"}
+        >
           Continue with Kakao
         </Button>
       </VStack>
