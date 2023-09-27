60310ceb8b736cc94a236d72372a0a020d67ad02
yoseph0524 <yoseph0524@g.ucla.edu>
2023-09-26 22:17:40 -0700

    manual fetching

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,3 +1,80 @@
+import { Box, Grid, HStack, Skeleton, SkeletonText } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import Room from "../components/Room";
+import RoomSkeleton from "../components/RoomSkeleton";
+
+interface IPhoto {
+  pk: string;
+  file: string;
+  description: string;
+}
+
+interface IRoom {
+  pk: number;
+  name: string;
+  country: string;
+  city: string;
+  price: number;
+  rating: number;
+  is_owner: boolean;
+  photos: IPhoto[];
+}
+
 export default function Home() {
-  return <span>homeeee</span>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [rooms, setRooms] = useState<IRoom[]>([]);
+  const fetchRooms = async () => {
+    const response = await fetch("http://127.0.0.1:8000/api/v1/rooms/");
+    const json = await response.json();
+    setRooms(json);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    fetchRooms();
+  }, []);
+  return (
+    <Grid
+      mt={10}
+      px={{
+        base: 10,
+        lg: 40,
+      }}
+      columnGap={4}
+      rowGap={8}
+      templateColumns={{
+        sm: "1fr",
+        md: "1fr 1fr",
+        lg: "repeat(3, 1fr)",
+        xl: "repeat(4, 1fr)",
+        "2xl": "repeat(5, 1fr)",
+      }}
+    >
+      {isLoading ? (
+        <>
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+        </>
+      ) : null}
+      {rooms.map((room) => (
+        <Room
+          imageUrl={
+            room.photos[0].file ?? "https://source.unsplash.com/random/450x450"
+          }
+          name={room.name}
+          rating={room.rating}
+          city={room.city}
+          country={room.country}
+          price={room.price}
+        />
+      ))}
+    </Grid>
+  );
 }
